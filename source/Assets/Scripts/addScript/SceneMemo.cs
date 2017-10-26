97cf5b72e0bbd35c23ecc77325b7d2a41b4322ad
TUmedu <wewe53540@gmail.com>
2017-10-26 18:21:57 +0900

    shared file
    ファイル共有しました

---
+++ source/Assets/Scripts/addScript/SceneMemo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMemo : MonoBehaviour {
+
+
+    //public static string sceneName = SceneManager.GetActiveScene().name;
+
+        //リスタートの為に前のシーン保存
+    public static string playScene()
+    {
+        return SceneManager.GetActiveScene().name;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+
+         
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
