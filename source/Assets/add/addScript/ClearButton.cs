9ce2ade054703ab36d334c12ccd1bd362853c68e
TUmedu <wewe53540@gmail.com>
2017-11-06 18:28:29 +0900

    クリア
    クリアイラスト３０msecするとMenu2
    タッチするとMenu2へ

---
+++ source/Assets/add/addScript/ClearButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearButton : MonoBehaviour {
+
+
+
+
+
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        Debug.Log("Button click!");
+        Application.LoadLevel("Menu2");
+
+    }
+}
+
---
+++ source/Assets/add/addScript/ClearSTOP.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearSTOP : MonoBehaviour {
+
+    public float timeOut;
+
+    void Start()
+    {
+        StartCoroutine(FuncCoroutine());
+    }
+
+    IEnumerator FuncCoroutine()
+    {
+        while (true)
+        {
+            // Do anythings
+
+            
+
+            yield return new WaitForSeconds(timeOut);
+
+            Application.LoadLevel("Menu2");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
